89bc264ad1c1ec9f1b1d5f3bd0e1979a2b5496c7
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-07-09 21:54:29 +0530

    curd opertion

---
+++ app.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+// const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+
+mongoose.connect(`mongodb+srv://#####################@cluster1.syv6z7h.mongodb.net/shap?retryWrites=true&w=majority`).then(result=>{
+  app.listen(4000)
+  console.log('server running ...')
+}).catch(err=>{
+  console.log("Server Not Responding")
+})
